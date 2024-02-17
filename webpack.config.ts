715b8634f39b6dd4e92e804c276dae112586e8fe
Scott Benton <scott@scottbenton.dev>
2024-02-17 15:43:23 -0500

    fix(build): Updated webpack config

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,16 +1,13 @@
 import { constructModuleWebpackConfig } from "@scottbenton/apps-build";
 import { ModuleScope } from "@scottbenton/apps-config";
 
-const config = constructModuleWebpackConfig(
-  {
-    name: ModuleScope.HomePage,
-    modules: [ModuleScope.Authentication],
-    dependencies: {},
-    exposes: {
-      "./HomePage": "./src/HomePage.tsx",
-    },
+const config = constructModuleWebpackConfig({
+  name: ModuleScope.HomePage,
+  modules: [ModuleScope.Authentication],
+  dependencies: {},
+  exposes: {
+    "./HomePage": "./src/HomePage.tsx",
   },
-  true
-);
+});
 
 export default config;
