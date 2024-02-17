898324db1080f454cc60f5dc0c8fd09edb28a0c3
Scott Benton <scott@scottbenton.dev>
2024-02-17 13:30:13 -0500

    feat(login): Added auth section to apps header

--- src/components/LoginSection.tsx
+++ src/components/LoginSection.tsx
@@ -5,10 +5,30 @@ import { Link } from "react-router-dom";
 export function LoginSection() {
   const { loading, user } = useCurrentUser();
   if (loading) {
-    return <span>Loading</span>;
+    return null;
   } else if (user) {
-    return <span>Logged in</span>;
+    if (user.email) {
+      return (
+        <div className={"flex flex-col px-4 py-1 border rounded-xl"}>
+          <span className={"text-xs font-semibold text-gray-600"}>
+            Logged in as
+          </span>
+          <span className={"text-gray-800"}>{user.email ?? "Loading..."}</span>
+        </div>
+      );
+    } else {
+      return null;
+    }
   } else {
-    return <Link to={"/auth"}>Log In</Link>;
+    return (
+      <Link
+        to={"/auth"}
+        className={
+          "bg-home-700 text-white px-4 py-2 font-bold rounded-xl hover:bg-home-800 focus:ring-2 ring-home-400 focus:outline-none transition-colors duration-300 ease-in-out"
+        }
+      >
+        Log In
+      </Link>
+    );
   }
 }
